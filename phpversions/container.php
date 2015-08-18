eebabb4376446d87fdbda6781c098fa3b8e0368e
taylor <taylor.zanke@gmail.com>
2015-08-18 16:19:05 -0400

    first commit

---
+++ phpversions/container.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title></title>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <link rel="stylesheet" type="text/css" href="style.css"/>
+    </head>
+    <body>
+
+    	<div class="container">
+			<div class="navigation">
+	    		<div class="textLeft">
+                    Search
+                </div>
+                <div class="textCenter">
+                    <a href="">
+					Visual Resources Collection <br>
+                    Graduate School of Architecture Planning and Preservation
+                    </a>
+                </div>
+                <div class="textRight">
+                    <ul>
+                        <li class="active"><a href="images_video.html">Resources</a></li>
+                        <li><a href="architects_projects.html">Index</a></li>
+                        <li><a href="about.html">About</a></li>
+                    </ul>
+                </div>
+		    </div>
+
+	    	<div class="pages">
+	    		<div class="pageLeft">
+	    			<div class="pageContent"></div>
+	    		</div>
+	    		<div class="pageRight">
+	    			<div class="pageContent"></div>
+	    		</div>
+	    	</div>
+	    </div><!--close .container div-->
+
+    </body>
+</html>
