9ef63e957668eda5e2b80788b43a9bc1ad789ba0
cameronhr <cwh2128@columbia.edu>
2015-08-26 00:57:26 -0400

    currentlink.js work

--- newsite/js/currentLink.js
+++ newsite/js/currentLink.js
@@ -1,4 +1,4 @@
-// Pasted from http://stackoverflow.com/questions/2397370/how-to-change-the-link-color-of-the-current-page-with-css, needs modification. Use to style current link in Nav Bar
+// Copied from http://stackoverflow.com/questions/2397370/how-to-change-the-link-color-of-the-current-page-with-css, needs modification. Use to style current link in Nav Bar
 
 $(document).ready(function() {
     $("[href]").each(function() {
