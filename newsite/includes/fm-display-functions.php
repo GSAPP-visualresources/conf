802996b699e4ad520ba7d93c56d3744b8a60a879
cameronhr <camhard@gmail.com>
2015-09-13 22:23:24 -0400

    above

--- newsite/includes/fm-display-functions.php
+++ newsite/includes/fm-display-functions.php
@@ -96,6 +96,7 @@ function fmDisplaySearchResults($result,$whetherToPaginate) {
 			if($record->getField('Source_Type')=="Images") {
 				displayImgPreview($record);
 			} 
+							}
 				displayImgPreview($record);
 				$fileType = "Images";
 			}
