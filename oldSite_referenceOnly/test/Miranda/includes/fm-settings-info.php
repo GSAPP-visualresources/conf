6512d0f73cbcce9bcd88c999722b2fbb9fb3b182
unknown <FileMaker@GSAPPVRC.gsapp.columbia.edu>
2015-09-25 10:10:43 -0400

    trying to sync

--- oldSite_referenceOnly/test/Miranda/includes/fm-settings-info.php
+++
@@ -1,8 +0,0 @@
-<?php
-$record = $fm->getRecordById('_Settings', 1);
-if (FileMaker::isError($record)) {
-    echo "<body>Error: " . $record->getMessage(). "</body>";
-    exit;
-}?>
-
-<p>This database was last synced on <?php echo $record->getField('Date_LastSync'); ?></p>
