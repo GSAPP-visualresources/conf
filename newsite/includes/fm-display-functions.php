cec361cdba7f7d03cfc9f70498da1e97e4143cf8
taylorz <taylorzanke@gmail.com>
2015-10-16 11:25:55 -0400

    adding change

--- newsite/includes/fm-display-functions.php
+++ newsite/includes/fm-display-functions.php
@@ -44,10 +44,7 @@ function displayImgPreview($record) {
 	global $querystringadded,$querykeywords;
 	$strOutput = "";
 	$strOutput .= '<a href="resource_detail.php?img_id=';
-<<<<<<< HEAD
-=======
-	$strOutput .= '<a class="HI" href="resource_detail.php?img_id=';
->>>>>>> 3cc4b201a376d5b4c4aa4407d8f90427766daa1d
+	$strOutput .= '<a href="resource_detail.php?img_id=';
 	$strOutput .= fmDisplayFieldResult('Filename',$record);
 	$strOutput .= $querystringadded.'"><div class="tilethumbcontainer"><img class="tilethumb" src="img/';
 	$strOutput .= 'thumbnails/'.basename($record->getField('Filepath_Thumbnail'));
